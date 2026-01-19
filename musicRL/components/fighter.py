bed71e55c782fb132ed05173a82d45fef89e884a
Cyan <studio@cyanmyk.com>
2026-01-19 13:39:35 +0000

    message log implementation

--- musicRL/components/fighter.py
+++ musicRL/components/fighter.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import color
 from components.base_component import BaseComponent
 from input_handlers import GameOverEventHandler
 from render_order import RenderOrder
@@ -31,9 +32,11 @@ class Fighter(BaseComponent):
     def die(self) -> None:
         if self.engine.player is self.entity:
             death_message = "You died!"
+            death_message_color = color.player_die
             self.engine.event_handler = GameOverEventHandler(self.engine)
         else:
             death_message = f"{self.entity.name} is dead!"
+            death_message_color = color.enemy_die
 
         self.entity.char = "%"
         self.entity.color = (191, 0, 0)
@@ -41,5 +44,3 @@ class Fighter(BaseComponent):
         self.entity.ai = None
         self.entity.name = f"remains of {self.entity.name}"
         self.entity.render_order = RenderOrder.CORPSE
-
-        print(death_message)
